5840ce0eaceac89d5a09e97a2e6123ee23c2c1b1
Pedro Ferreira <pedro.ferreira@corporativo.octo>
2018-05-14 13:15:30 -0300

    Cleaning unnecessary methods

--- API/API/Controllers/WorkItemsController.cs
+++ API/API/Controllers/WorkItemsController.cs
@@ -10,29 +10,6 @@ namespace API.Controllers
 {
     public class WorkItemsController : ApiController
     {
-        //https://docs.microsoft.com/pt-br/aspnet/web-api/overview/getting-started-with-aspnet-web-api/tutorial-your-first-web-api
-        Product[] products = new Product[]
-       {
-            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
-            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
-            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
-       };
-
-        public IEnumerable<Product> GetAllProducts()
-        {
-            return products;
-        }
-
-        public IHttpActionResult GetProduct(int id)
-        {
-            var product = products.FirstOrDefault((p) => p.Id == id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-            return Ok(product);
-        }
-
         [HttpGet]
         public IHttpActionResult GetPage(int pageNumber)
         {
